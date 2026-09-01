8ed1e02eed33424cf09bcfd5f9f0dc0f269d21f9
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-01 18:50:39 +0300

    Add a blank line for improved code readability

--- src/main/java/org/cristian/application/HelloWorldController.java
+++ src/main/java/org/cristian/application/HelloWorldController.java
@@ -3,6 +3,7 @@ package org.cristian.application;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloWorldController {
     @GetMapping(path="/hello")
