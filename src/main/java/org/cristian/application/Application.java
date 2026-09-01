ae437083c4e5c3bbd853b7c0f4f189e46f8ec506
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-01 18:50:36 +0300

    Add a blank line for improved code readability

--- src/main/java/org/cristian/application/Application.java
+++ src/main/java/org/cristian/application/Application.java
@@ -3,6 +3,7 @@ package org.cristian.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
 
