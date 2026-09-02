62816d95d6840fbe551553cd0538d399b1583e68
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-02 07:27:26 +0300

    Add a blank line for improved code readability

--- src/test/java/org/cristian/application/ApplicationTests.java
+++ src/test/java/org/cristian/application/ApplicationTests.java
@@ -3,6 +3,7 @@ package org.cristian.application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class ApplicationTests {
     @Test
