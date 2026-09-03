a74adc64fdd656c882df5d4c4633f21aaaf31735
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-03 11:07:34 +0300

    Add endpoint to retrieve a specific task by ID

--- src/main/java/org/cristian/application/TasksController.java
+++ src/main/java/org/cristian/application/TasksController.java
@@ -1,8 +1,5 @@
 package org.cristian.application;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +17,11 @@ public class TasksController {
     public List<Task> getTasks(){
        return tasks;
     }
+    @GetMapping("/api/tasks/{id}")
+    public Task getTask(@PathVariable int id){
+        return tasks.get(id);
+    }
+
     @PostMapping("/api/tasks")
     public Task addTask(@RequestBody Task t) {
         tasks.add(t);
