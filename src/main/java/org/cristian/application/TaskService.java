1b035575226e46c764f48d791b281795782abb5d
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-03 11:38:27 +0300

    Add endpoint to retrieve a specific task by ID

--- src/main/java/org/cristian/application/TaskService.java
+++ src/main/java/org/cristian/application/TaskService.java
@@ -1,8 +1,11 @@
 package org.cristian.application;
 
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TaskService {
+    public List<Task> tasks = new ArrayList<Task>();
 
 }
