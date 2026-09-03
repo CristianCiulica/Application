1225977a8ff5c689e0b01e8ea76c833e29e5ab95
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-03 10:44:50 +0300

    Update TasksController to return list of tasks from API endpoint

--- src/main/java/org/cristian/application/TasksController.java
+++ src/main/java/org/cristian/application/TasksController.java
@@ -2,10 +2,15 @@ package org.cristian.application;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TasksController {
+    public List<Task> tasks;
+
     @GetMapping("/api/tasks")
-    public String message(){
-        return "Hello Tasks";
+    public List<Task> getTasks(){
+       return tasks.stream()
+               .toList();
     }
 }
