79567eec435c55455be35cbe4631cff9d0d9ab97
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-03 16:51:28 +0300

    Refactor TasksController to use TaskService for task management

--- src/main/java/org/cristian/application/TaskService.java
+++ src/main/java/org/cristian/application/TaskService.java
@@ -3,6 +3,7 @@ package org.cristian.application;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -12,5 +13,26 @@ public class TaskService {
         tasks.add(new Task(1,"task1"));
         tasks.add(new Task(2,"task2"));
         tasks.add(new Task(3,"task3"));
+    }
+
+    public List<Task> getTasks(){
+        return tasks;
+    }
+
+    public Task addTask(Task task){
+        tasks.add(task);
+        return task;
+    }
+
+    public boolean deleteTask(int id){
+        return tasks.removeIf(task -> task.getId()==id);
+
+    }
+
+    public Optional<Task> getTask(int id){
+        var t = tasks.stream()
+                .filter(task -> task.getId()==id)
+                .findFirst();
+        return t;
     }
 }
--- src/main/java/org/cristian/application/TasksController.java
+++ src/main/java/org/cristian/application/TasksController.java
@@ -8,43 +8,38 @@ import java.util.Optional;
 
 @RestController
 public class TasksController {
-
-    public TasksController() {
-        tasks.add(new Task(1,"task1"));
-        tasks.add(new Task(2,"task2"));
-        tasks.add(new Task(3,"task3"));
+    private final TaskService service;
+    public TasksController(TaskService service){
+        this.service=service;
     }
 
     @GetMapping("/api/tasks")
     public List<Task> getTasks(){
-       return tasks;
+       return service.getTasks();
     }
 
     @GetMapping("/api/tasks/{id}")
     public ResponseEntity<Task> getTask(@PathVariable int id){
-       var t = tasks.stream()
-               .filter(task -> task.getId()==id)
-               .findFirst();
+       var t = service.getTask(id);
        if(t.isEmpty())
            return ResponseEntity.notFound().build();
        else
            return ResponseEntity.ok(t.get());
 
     }
+
     @PostMapping("/api/tasks")
     public Task addTask(@RequestBody Task t) {
-        tasks.add(t);
-        return t;
+       return service.addTask(t);
     }
 
+
     @DeleteMapping("/api/tasks/{id}")
-    public ResponseEntity<Task> deleteTask(@PathVariable int id){
-        if(tasks.removeIf(task -> task.getId()==id))
+    public ResponseEntity<Void> deleteTask(@PathVariable int id){
+        if(service.deleteTask(id))
             return ResponseEntity.noContent().build();
         else
             return ResponseEntity.notFound().build();
-
-
     }
 
 }
