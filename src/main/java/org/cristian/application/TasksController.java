6896f3f1f25d57ba6f11eb6e51409edf2858a003
Cristian <cristian.ciulica@student.unitbv.ro>
2026-09-03 10:37:36 +0300

    Add TasksController with API endpoint for tasks

--- src/main/java/org/cristian/application/TasksController.java
+++ src/main/java/org/cristian/application/TasksController.java
@@ -5,5 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TasksController {
     @GetMapping("/api/tasks")
-
+    public String message(){
+        return "Hello Tasks";
+    }
 }
